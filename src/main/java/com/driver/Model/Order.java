555b43709a5da1a7e923069a4431bcc8e3dc885e
Sangmeshwar19 <118285812+Vyankateshwar@users.noreply.github.com>
2022-12-24 21:04:57 +0530

    Initial commit

---
+++ src/main/java/com/driver/Model/Order.java
@@ -0,0 +1,23 @@
+package com.driver.Model;
+
+public class Order {
+
+    private String id;
+    private int deliveryTime;
+
+    public Order(String id, String deliveryTime) {
+
+        // The deliveryTime has to converted from string to int and then stored in the attribute
+        //deliveryTime  = HH*60 + MM
+        this.id = id;
+        String[] list = deliveryTime.split(":");
+        this.deliveryTime = (Integer.parseInt(list[0])*60) + Integer.parseInt(list[1]);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public int getDeliveryTime() {return deliveryTime;}
+
+}
